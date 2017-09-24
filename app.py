56604530c2adaa0a01ee52c881d7d7e45f4f6097
marton bognar <martonbognar@sch.bme.hu>
2017-09-24 05:48:11 +0200

    Increase request timeouts and improve coding style

--- app.py
+++ app.py
@@ -1,32 +1,78 @@
 import requests
 from requests.exceptions import RequestException
 from flask import Flask, jsonify
-app = Flask(__name__)
 
-team = ('cica', [{'name': 'Rabinovits Jakov'}, {'name': 'Bognar Marton'}, {'name': 'Tusko Gergely'}])
-teams_url = 'https://chaosstack-starter.herokuapp.com/teams?hash=FMJB6A5JXTMsJuULE9fpq94mHgn2'
 
-@app.route('/team-members')
+
+APP = Flask(__name__)
+
+TEAM = {
+    "teamName": "cica",
+    "members": [
+        {
+            "name": "Rabinovits Jakov",
+        },
+        {
+            "name": "Tuskó Gergely",
+        },
+        {
+            "name": "Bognár Márton",
+        },
+    ]
+}
+
+TEAMS_URL = 'https://chaosstack-starter.herokuapp.com/teams?hash=FMJB6A5JXTMsJuULE9fpq94mHgn2'
+
+def set_encoding(response):
+    """Set the response's content type and encoding"""
+
+    response.headers["Content-Type"] = "text/json; charset=utf-8"
+    return response
+
+
+@APP.route('/team-members')
 def get_team_members():
-    return jsonify(members=team[1], teamName=team[0])
+    """Return the list of the team members"""
 
-@app.route('/competitors')
+    return set_encoding(jsonify(TEAM))
+
+
+@APP.route('/competitors')
 def get_competitors():
-    urls = requests.get(teams_url)
-    teams = []
+    """Return the names of all the teams in the competition (if their API
+    responds in time)"""
+
+    teams = [
+        {
+            "name": TEAM["teamName"],
+        },
+    ]
+
+    try:
+        urls = requests.get(TEAMS_URL)
+    except RequestException:
+        print("Failed to fetch teams url")
+        return set_encoding(jsonify(teams=teams))
+
     for url in urls.json():
+        full_url = '/'.join([url['url'].strip('/'), 'team-members'])
+
         try:
-            fullUrl = '/'.join([url['url'].strip('/'), 'team-members'])
-            team_request = requests.get(fullUrl, timeout=0.1)
+            team_request = requests.get(full_url, timeout=2)
+        except RequestException:
+            print("Failed to load url", full_url)
+        else:
             if team_request.status_code == 200:
                 try:
                     req_team = team_request.json()
-                    teams.append({'name': req_team['teamName']})
                 except ValueError:
-                    pass
-        except RequestException:
-            pass
-    return jsonify(teams=teams)
+                    print("Invalid format for url", full_url)
+                else:
+                    teams.append({'name': req_team['teamName']})
+                    print("Successfully parsed", full_url)
+
+    return set_encoding(jsonify(teams=teams))
 
 if __name__ == '__main__':
-    app.run()
+    APP.config['JSON_AS_ASCII'] = False
+    APP.run()
